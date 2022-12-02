c30831e6a419f6ea04013d6901884508863ff747
Muhamad <92302413+MuhamadBoboev@users.noreply.github.com>
2022-12-02 19:47:17 +0300

    refesh project

--- js/script.js
+++ js/script.js
@@ -16,10 +16,23 @@ window.onload = function () {
     }
   });
 
+  window.addEventListener("click", (e) => {
+    console.log(e.target);
+    if (e.target == !document.querySelector("header__bl-logo")) {
+      console.log("asdsad");
+    }
+  });
   navLinks.forEach((el) => {
+    // console.log(tar.target);
+    // console.log("asasdsda", el);
+    // console.log(tar.target == document.querySelector(".nav__link__a"));
     el.addEventListener("click", (e) => {
-      el.classList.toggle("active");
-      el.querySelector(".bl-nav__link").classList.toggle("active");
+      // console.log(e.target);
+      // e.classList.add("active");
+      // console.log(e.querySelector(".bl-nav__link"));
+      // el.querySelector(".bl-nav__link").classList.toggle("active");
     });
+    // el.classList.add("active");
+    // el.querySelector(".bl-nav__link").classList.add("active");
   });
 };
